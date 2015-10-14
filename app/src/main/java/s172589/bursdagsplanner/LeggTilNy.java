9aae31c068e69559c70c78af01874b2a96855b3f
halvorR <halvorr91@gmail.com>
2015-10-14 08:10:48 +0200

    Fiksa klikkbar lagreknapp

--- app/src/main/java/s172589/bursdagsplanner/LeggTilNy.java
+++ app/src/main/java/s172589/bursdagsplanner/LeggTilNy.java
@@ -3,17 +3,32 @@ package s172589.bursdagsplanner;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
  * Created by Halvor on 13.10.2015.
  */
 public class LeggTilNy extends AppCompatActivity {
+
+    private String navn, dato;
+    private int telefonNr;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leggtilny);
+
+        Button lagreKnapp = (Button) findViewById(R.id.lagreNy);
+        lagreKnapp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                leggTil();
+            }
+        });
     }
+
 
     // Legg til person
     public void leggTil(){
