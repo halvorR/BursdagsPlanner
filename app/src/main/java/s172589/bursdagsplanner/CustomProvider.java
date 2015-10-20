53b79d77eaacfd53a4efe549b90597a6ba45eea7
halvorR <halvorr91@gmail.com>
2015-10-20 13:04:26 +0200

    Forsøk på ContentProvider

--- app/src/main/java/s172589/bursdagsplanner/CustomProvider.java
+++ app/src/main/java/s172589/bursdagsplanner/CustomProvider.java
@@ -2,13 +2,29 @@ package s172589.bursdagsplanner;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
+
+import java.util.List;
 
 /**
  * Created by Roger on 20.10.2015.
  */
 public class CustomProvider extends ContentProvider{
+    public static final String PROVIDER = "s172589.contentproviderbok";
+    public static final int KONTAKTER = 1;
+    public static final int KONTAKTER_ID = 2;
+    public static DBHandler db;
+
+    public static final Uri CONTENT_URI = Uri.parse("content://"+ PROVIDER + "/ " + db.TABLE_KONTAKTER);
+    private static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER, db.TABLE_KONTAKTER,KONTAKTER_ID);
+        uriMatcher.addURI(PROVIDER, db.TABLE_KONTAKTER+"/#",KONTAKTER);
+    }
+
     @Override
     public boolean onCreate() {
         return false;
